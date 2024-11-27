c45dcf46e5f02c1f28fe2dd952249fb14733c1c8
akempny <akempny@amusys.pl>
2024-11-27 15:59:03 +0100

    feat(init): add login form

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,11 +1,13 @@
 import Grid from "@mui/material/Grid2";
 import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 const LoginForm = () => {
   return (
     <Grid container>
       <TextField id="outlined-basic" label="Outlined" variant="standard" />
       <TextField id="filled-basic" label="Filled" variant="standard" />
+      <Button variant="contained">Zaloguj</Button>
     </Grid>
   );
 };
