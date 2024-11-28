99b18aac9213ce5f5e8ffa6d45270225659dbc10
arturkempny <akempny@amusys.pl>
2024-11-28 20:24:34 +0100

    add prettier and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import "./App.css";
+import './App.css'
 // import LoginPage from "./features/auth/login-page";
-import MainAppLayout from "layout/main-app-layout";
-import Sidebar from "features/sidebar/sidebar";
+import MainAppLayout from 'layout/main-app-layout'
+import Sidebar from 'features/sidebar/sidebar'
 
 function App() {
   return (
     <MainAppLayout>
       <Sidebar />
     </MainAppLayout>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,6 @@
+import { Routes, Route } from "react-router";
+const Routes = () => {
+    return (<Routes></Routes>)
+}
+
+export default Routes
