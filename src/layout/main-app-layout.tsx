b0b0114dfc2bc8b9dd8fff9238b4fbbc4d08568c
arturkempny <akempny@amusys.pl>
2024-11-29 08:57:26 +0100

    add main app layout

--- src/layout/main-app-layout.tsx
+++ src/layout/main-app-layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Box } from "@mui/material";
 import FullPageLayout from "./full-page-layout";
 // import { Outlet } from "react-router-dom";
@@ -7,8 +8,13 @@ const MainAppLayout = ({ children }: { children: React.ReactNode }) => {
     <FullPageLayout>
       <Box display="flex" width="100%">
         <Box>{children}</Box>
-        <Box className="main-container-page">
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', overflow: 'hidden' }}>
+          <Box sx={{ height: '64px', background: 'red', padding: '5px 15px 5px 15px' }}>
+            <div>navbar</div>
+          </Box>
+          <Box className="main-container-page" sx={{ background: 'green' }}>
           <p>main layout</p>
+          </Box>
           {/* <Outlet /> */}
         </Box>
       </Box>
